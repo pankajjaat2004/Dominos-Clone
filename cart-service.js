70c176fd9442ac20a8bf8ef534ed5b0277e876f7
Pankaj Kumar <135860999+pankajjaat2004@users.noreply.github.com>
2024-07-18 15:22:27 +0530

    Update cart-service.js

--- cart-service.js
+++ cart-service.js
@@ -1,20 +1,13 @@
-var arr=['Recommended','NewLaunches','VegPizza','GourmetPizza','NonVegPizza','Beverages','GarlicBread','PizzaMania','ValueCombos','Desserts','CheeseBurstPizza','SpicyPizza'];
-
 const cartOperations = {
     pizzas:[],
-addInCart(pizzaId,locationId){
-    // console.log(pizzas);
-
-    const pizza = this.pizzas[locationId].find(currentPizza=>currentPizza.id == pizzaId);
+addInCart(pizzaId){
+    const pizza = this.pizzas.find(currentPizza=>currentPizza.id == pizzaId);
     pizza.isInCart=true;
-    console.log(pizza);
 },
 
 viewAll(){
-    // for(let i=0;i<arr.length;i++){
         
-    return this.pizzas[arr[0]].filter(pizza=>pizza.isInCart);
-    // }
+    return this.pizzas.filter(pizza=>pizza.isInCart);
     
 }
 }
